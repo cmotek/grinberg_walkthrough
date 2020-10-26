3630cdf386824688ceac49806eea28e536609d38
cmotek <38844285+cmotek@users.noreply.github.com>
2020-10-25 20:14:55 -0700

    chapsixwork

--- flask_practice/hello.py
+++ flask_practice/hello.py
@@ -9,7 +9,8 @@ from wtforms.validators import DataRequired
 import os
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-
+from flask_mail import Mail
+from flask_mail import Message
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -21,10 +22,16 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
+app.config['FLASK_MAIL_SENDER'] = 'Flasky Admin <flasky@example.com>'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
-
 migrate = Migrate(app,db)
+mail = Mail(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -83,4 +90,8 @@ class User(db.Model):
     def __repr__(self):
         return '<USER %r>' % self.username
 
-    
+def send_email(to, subject, template, **kwargs):
+    msg = Message(app.config['FLASK_MAIL_SUBJECT_PREFIX'] + subject, sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    mail.send(msg)    
