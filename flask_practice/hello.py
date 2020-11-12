ddff706cbe0e4fef9ec0ba6b07a8fd8b7830edec
cmotek <38844285+cmotek@users.noreply.github.com>
2020-11-11 21:16:41 -0800

    chapsixwork

--- flask_practice/hello.py
+++ flask_practice/hello.py
@@ -24,6 +24,7 @@ db = SQLAlchemy(app)
 app.config['SECRET_KEY'] = 'hard to guess string'
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = 1
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
