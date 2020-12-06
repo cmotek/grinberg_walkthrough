01db1771444de28f9c5ea26e06d86751dfc63cd8
cmotek <38844285+cmotek@users.noreply.github.com>
2020-12-05 20:55:29 -0800

    chapeightwork

--- flask_practice/app/__init__.py
+++ flask_practice/app/__init__.py
@@ -3,13 +3,14 @@ from flask_bootstrap import Bootstrap
 from flask_mail import Mail
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
-from config import config
 from flask_login import LoginManager
+from config import config
 
 bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
+
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
 
--- flask_practice/app/auth/views.py
+++ flask_practice/app/auth/views.py
@@ -1,9 +1,8 @@
 from flask import render_template, redirect, request, url_for, flash
-from flask_login import login_user
+from flask_login import login_user, logout_user, login_required
 from . import auth
 from ..models import User
 from .forms import LoginForm
-from flask_login import logout_user, login_required
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -11,7 +10,7 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user is not None and user.verify_password(form.password.data):
-            login_user(user, form.remeber_me.data)
+            login_user(user, form.remember_me.data)
             next = request.args.get('next')
             if next is None or not next.startswith('/'):
                 next = url_for('main.index')
@@ -19,7 +18,7 @@ def login():
         flash('Invalid username or passsword.')
     return render_template('auth/login.html')
 
-@auth.rout('/logout')
+@auth.route('/logout')
 @login_required
 def logout():
     logout_user()
