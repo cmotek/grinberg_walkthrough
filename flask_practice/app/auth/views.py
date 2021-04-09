9048f6ebd2450e3f59e0535d54acc3b00aab939e
cmotek <38844285+cmotek@users.noreply.github.com>
2021-04-08 21:42:16 -0700

    chaptenstruggle

--- flask_practice/app/auth/views.py
+++ flask_practice/app/auth/views.py
@@ -7,6 +7,22 @@ from ..models import User
 from ..email import send_email
 from .forms import LoginForm, RegistrationForm
 
+@auth.before_app_request
+def before_request():
+      if current_user.is_authenticated:
+        current_user.ping()
+        if not current_user.confirmed \
+                and request.endpoint \
+                and request.blueprint != 'auth' \
+                and request.endpoint != 'static':
+            return redirect(url_for('auth.unconfirmed'))
+
+@auth.route('/unconfirmed')
+def unconfirmed():
+    if current_user.is_anonymous or current_user.confirmed:
+        return redirect(url_for('main.index'))
+    return render_template('auth/unconfirmed.html')
+
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
@@ -20,6 +36,13 @@ def login():
             return redirect(next)
         flash('Invalid username or passsword.')
     return render_template('auth/login.html', form=form)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.')
+    return redirect(url_for('main.index'))
 
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
@@ -49,21 +72,6 @@ def confirm(token):
         flash('The confirmation link is invalid or has expired.')
     return redirect(url_for('main.index'))
 
-@auth.before_app_request
-def before_request():
-      if current_user.is_authenticated:
-        current_user.ping()
-        if not current_user.confirmed \
-                and request.endpoint \
-                and request.blueprint != 'auth' \
-                and request.endpoint != 'static':
-            return redirect(url_for('auth.unconfirmed'))
-
-@auth.route('/unconfirmed')
-def unconfirmed():
-    if current_user.is_anonymous or current_user.confirmed:
-        return redirect(url_for('main.index'))
-    return render_template('auth/unconfirmed.html')
 
 @auth.route('/confirm')
 @login_required
@@ -75,9 +83,3 @@ def resend_confirmation():
     return redirect(url_for('main.index'))
 
 
-@auth.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    flash('You have been logged out.')
-    return redirect(url_for('main.index'))
